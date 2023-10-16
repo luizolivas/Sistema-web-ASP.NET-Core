185d98c4faf739e870818b914cd96133a71d7482
Técnico Lift <tecnicolift@hotmail.com>
2023-10-16 12:06:54 -0300

    Seed DB

---
+++ PessoalMvcSales/Data/PessoalMvcSalesContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PessoalMvcSales.Models;
+
+namespace PessoalMvcSales.Data
+{
+    public class PessoalMvcSalesContext : DbContext
+    {
+        public PessoalMvcSalesContext (DbContextOptions<PessoalMvcSalesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+    }
+}
